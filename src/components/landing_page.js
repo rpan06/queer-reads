4080307df7f237890d5c80e2c13600c138b18207
Rachel Pan <rpan06@gmail.com>
2018-09-26 18:11:45 -0700

    installed router

---
+++ src/components/landing_page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class LandingPage extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>this is landing page</h1>
+            </div>
+        )
+    }
+}
