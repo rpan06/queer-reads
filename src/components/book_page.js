c3e3ecfe8931ae1c0d074fa14760143b9b40f8e5
Rachel Pan <rpan06@gmail.com>
2018-09-26 21:00:56 -0700

    routed pages and isbn book pages

--- src/components/book_page.js
+++ src/components/book_page.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
 export default class BookPage extends React.Component {
+    state = {}
+
+    componentDidMount(){
+        this.getBookDetails();
+    }
+
+    getBookDetails(){
+        const {ISBN} = this.props.match.params;
+        $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(`https://www.goodreads.com/book/isbn/${ISBN}`) + '&callback=?', function(data){
+            this.scrapeWebData()
+        });
+    }
+
+    scrapeWebData(){
+        const imageURL = data.match(/id="coverImage"[\w\W]*(http.*\.jpg)[\w\W]*class="bookCoverActions">/)[1]
+        const bookInfo = data.match(/id="bookTitle[\w\W]*itemprop="name">\n(.*)\n[\w\W]*>\n(.*)\n<\/a><\/h2>[\w\W]*href="(.*)"><span itemprop="name">(.*)<\/span><\/a>[\w\W]*class="authorName__container/);
+        const title = bookInfo[1].trim();
+        const series = bookInfo[2].trim();
+        const authorURL = bookInfo[3];
+        const author = bookInfo[4];
+        const bookDescription = data.match(/id="description"[\w\W]*freeTextContainer[\d]*">(.*)<\/span>[\w\W]*style="display:none">(.*)<\/span>[\w\W]*buyButtonContainer/);
+        const shortDescription = bookDescription[1];
+        const longDescription = bookDescription[2];
+        this.setState({
+            imageURL: imageURL,
+            title: title,
+            series: series,
+            author: author,
+            longDescription: longDescription
+        })
+    }
+
     render(){
-        // const {Title,series,author,imageUrl,shortDescription} = this.props.item
+        const {imageURL, title, series, author, longDescription} = this.state;
         return(
             <div id="bookpage" className="container">
                 <div className="row">
@@ -10,7 +42,7 @@ export default class BookPage extends React.Component {
                 </div>
                 <div className="row">
                     <div id="book-information" className="col s3 center">
-                        <img src="./images/7235533.jpg"/>
+                        <img src={imageURL}/>
                         <h4>Rating</h4>
                         <p>4.65 stars</p>
 
@@ -22,12 +54,11 @@ export default class BookPage extends React.Component {
                     </div>
 
                     <div className="col s9">
-                        <h4>The Way of Kings</h4>
-                        <h4>(The Stormlight Archive, #1)</h4>
-                        <h4>by Brandon Sanderson</h4>
+                        <h4>{title}</h4>
+                        <h4>{series}</h4>
+                        <h4>by {author}</h4>
                         <br/>
-                        <p>From #1 New York Times bestselling author Brandon Sanderson, The Way of Kings, Book One of the Stormlight Archive begins an incredible new saga of epic proportion.</p>
-                        <p>Roshar is a world of stone and storms. Uncanny tempests of incredible power sweep across the rocky terrain so frequently that they have shaped ecology and civilization alike. Animals hide in shells, trees pull in branches, and grass retracts into the soilless ground. Cities are built only where the topography offers shelter.</p>
+                        <p>{longDescription}</p>
                     </div>
                 </div>
             </div>
---
+++ src/components/not_found.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+export default props => {
+    const container = {
+        height: '100vh',
+        display: 'flex'
+    }
+
+    const content = {
+        margin: 'auto',
+        textAlign: 'center'
+    }
+
+    return (
+        <div style={container}>
+            <div style={content}>
+                <h1>404 PAGE NOT FOUND REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</h1>
+                <p>Invalid route</p>
+                <Link to="/">Back To Home</Link>
+            </div>
+        </div>
+    )
+}
